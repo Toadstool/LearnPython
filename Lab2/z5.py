01055e20ec94e8bcdb982f42997ff3421569e44c
Toadstool <mark.toadstool@gmail.com>
2018-10-27 15:03:08 +0200

    z5

--- Lab2/z5.py
+++ Lab2/z5.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-A=np.array([[1,2,3,4],[5,6,7,8],[7,8,9,11],[2,3,6,4]])
+#A=np.array([[1,2,3,4],[5,6,7,8],[7,8,9,11],[2,3,6,4]])
+A = np.random.rand(100,100)
 print(A)
-b=np.array(range(1,5))
+b=np.array(range(1,101))
 print(b)
 x=np.linalg.solve(A,b)
+print(x)
+
+(A,b)
 print(x)
 
 
