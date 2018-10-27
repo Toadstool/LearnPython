2305d0ec8ad2d0b6a7b47a78c6462ed6b628ff23
Toadstool <mark.toadstool@gmail.com>
2018-10-27 14:01:34 +0200

    z3

---
+++ Lab2/z3.py
@@ -0,0 +1,17 @@
+import random
+
+
+
+def waga(l,w,p,wyn):
+   if w==0: return wyn
+   if w<0: return False
+   if(p==len(l)): return False
+   
+   return  waga(l,w,p+1,wyn) or waga(l,w-l[p],p+1,wyn + [l[p]]) or waga(l,w+l[p],p+1,wyn + [l[p]])
+   
+   
+
+L = [1,2,5,10,23]
+for i in range(0,50):
+   r= waga(L,i,0,[])
+   print("%s %s" % ( i,r))
